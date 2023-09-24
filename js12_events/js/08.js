2e4a446ac3e542f0df7a61b7dc790bf7bf6747b8
YEOBLEE <mauyeoblee@gmail.com>
2023-09-24 20:07:03 +0900

    Update 08.js

--- js12_events/js/08.js
+++ js12_events/js/08.js
@@ -6,5 +6,7 @@ $(function() {
 			$(this).siblings('li').find('ul').hide();
 			$(this).addClass('on').siblings('li').removeClass('on');
 
-		}
+		}).on('mouseleave', function() {
+			$('#gnb ul').hide().add('#gnb .n').removeClass('on');
+		})
 })
